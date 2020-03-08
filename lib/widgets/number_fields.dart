13b87c9089a62e8a1af2d2c8615322f41a88434d
Rodrigo Tavares <rodrigost23@gmail.com>
2020-03-08 18:00:02 -0400

    Add loading skeleton for result

--- lib/widgets/number_fields.dart
+++ lib/widgets/number_fields.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:money2/money2.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 import '../bloc/converter_bloc.dart';
 
@@ -16,9 +17,9 @@ class _NumberFieldsState extends State<NumberFields> {
   Widget build(BuildContext context) {
     var inactiveText = Theme.of(context).textTheme.display2;
     var activeText = inactiveText.copyWith(color: Theme.of(context).textTheme.body1.color);
+    var result = Money.fromInt(0, BlocProvider.of<ConverterBloc>(context).state.toCurrency);
 
     return BlocBuilder<ConverterBloc, ConverterState>(builder: (context, snapshot) {
-      var result = Money.fromInt(0, snapshot.toCurrency);
       if (snapshot is ConverterResulted) {
         result = snapshot.result;
       }
@@ -47,14 +48,31 @@ class _NumberFieldsState extends State<NumberFields> {
               padding: EdgeInsets.symmetric(horizontal: 24),
               height: 100,
               alignment: AlignmentDirectional.centerEnd,
-              child: AnimatedOpacity(
-                opacity: snapshot is ConverterResulted ? 1 : 0,
-                duration: Duration(milliseconds: 100),
-                child: Text(
-                  result.toString(),
-                  textAlign: TextAlign.end,
-                  style: activeText,
-                ),
+              child: AnimatedSwitcher(
+                duration: Duration(milliseconds: 200),
+                child: snapshot is ConverterLoading
+                    ? SkeletonAnimation(
+                        child: Container(
+                          child: Text(
+                            result.toString(),
+                            textAlign: TextAlign.end,
+                            style: activeText.copyWith(color: Colors.transparent),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(8)
+                          ),
+                        ),
+                      )
+                    : AnimatedOpacity(
+                        opacity: snapshot is ConverterResulted ? 1 : 0,
+                        duration: Duration(milliseconds: 100),
+                        child: Text(
+                          result.toString(),
+                          textAlign: TextAlign.end,
+                          style: activeText,
+                        ),
+                      ),
               ),
             ),
           ],
