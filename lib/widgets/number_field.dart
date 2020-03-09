d5d8d63dd32736385ba59ad1755cec8b8a65bd84
Rodrigo Tavares <rodrigost23@gmail.com>
2020-03-09 18:50:54 -0400

    Long-press to copy values

--- lib/widgets/number_field.dart
+++ lib/widgets/number_field.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:money2/money2.dart';
 import 'package:skeleton_text/skeleton_text.dart';
 
@@ -12,6 +13,7 @@ class NumberField extends StatelessWidget {
   final bool hidden;
   final Currency currency;
   final Function(Currency) onCurrencySelected;
+  final Function() onTap;
 
   const NumberField({
     Key key,
@@ -21,6 +23,7 @@ class NumberField extends StatelessWidget {
     this.color,
     this.loading = false,
     this.hidden = false,
+    this.onTap,
   }) : super(key: key);
 
   @override
@@ -31,7 +34,7 @@ class NumberField extends StatelessWidget {
         .copyWith(color: color ?? Theme.of(context).textTheme.body1.color);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsetsDirectional.only(start: 24, end: 12),
       child: Row(
         children: <Widget>[
           CurrencyPicker(
@@ -39,35 +42,47 @@ class NumberField extends StatelessWidget {
             onChanged: onCurrencySelected,
           ),
           Expanded(
-            child: Container(
-              padding: EdgeInsetsDirectional.only(start: 8),
-              alignment: AlignmentDirectional.centerEnd,
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 200),
-                child: loading
-                    ? SkeletonAnimation(
-                        child: Container(
-                          child: Text(
+            child: InkWell(
+              child: Container(
+                padding: EdgeInsetsDirectional.only(start: 8, end: 12),
+                alignment: AlignmentDirectional.centerEnd,
+                child: AnimatedSwitcher(
+                  duration: Duration(milliseconds: 200),
+                  child: loading
+                      ? SkeletonAnimation(
+                          child: Container(
+                            child: Text(
+                              text,
+                              textAlign: TextAlign.end,
+                              style: textStyle.copyWith(color: Colors.transparent),
+                            ),
+                            decoration: BoxDecoration(
+                                color: Colors.grey[300], borderRadius: BorderRadius.circular(8)),
+                          ),
+                        )
+                      : AnimatedOpacity(
+                          opacity: hidden ? 0 : 1,
+                          duration: Duration(milliseconds: 100),
+                          child: AutoSizeText(
                             text,
                             textAlign: TextAlign.end,
-                            style: textStyle.copyWith(color: Colors.transparent),
+                            softWrap: true,
+                            style: color != null ? textStyle.copyWith(color: color) : textStyle,
+                            maxLines: 1,
                           ),
-                          decoration:
-                              BoxDecoration(color: Colors.grey[300], borderRadius: BorderRadius.circular(8)),
                         ),
-                      )
-                    : AnimatedOpacity(
-                        opacity: hidden ? 0 : 1,
-                        duration: Duration(milliseconds: 100),
-                        child: AutoSizeText(
-                          text,
-                          textAlign: TextAlign.end,
-                          softWrap: true,
-                          style: color != null ? textStyle.copyWith(color: color) : textStyle,
-                          maxLines: 1,
-                        ),
-                      ),
+                ),
               ),
+              borderRadius: BorderRadius.circular(4),
+              onTap: onTap,
+              onLongPress: !hidden ? () {
+                Clipboard.setData(new ClipboardData(text: text));
+                Scaffold.of(context).showSnackBar(new SnackBar(
+                  content: new Text("Copiado: $text"),
+                  behavior: SnackBarBehavior.floating,
+                  duration: Duration(seconds: 1),
+                ));
+              } : null,
             ),
           ),
         ],
